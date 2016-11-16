308124d55c27c802966a16636110af8aedfe7ed1
arkki <arkki@arkki.tech>
2016-11-16 12:00:23 +1100

    new function for CDing up until the given folder

---
+++ fish/.config/fish/functions/cdup.fish
@@ -0,0 +1,20 @@
+function cdup --description 'CD up until given folder'
+	set pwd (pwd);
+	set target_folder $argv[1];
+
+	if [ ! (string match -r '/'$target_folder'/' $pwd) ]
+		echo Folder $target_folder is not in the pwd: $pwd;
+		return;
+	end;
+
+	set folder_arr (string split '/' $pwd);
+	set folder_count 1;
+	for folder in $folder_arr
+		if [ $folder != $target_folder ]
+			set folder_count (math $folder_count + 1);
+		else
+			cd (string join '/' $folder_arr[1..$folder_count]);
+			return;
+		end;
+	end;
+end;
