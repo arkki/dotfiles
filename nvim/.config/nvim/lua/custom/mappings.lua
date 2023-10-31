00b576938c694706b21ce9ae0e7403c57bf8d446
arkki <arkki@arkki.casa>
2023-10-31 23:42:01 +0200

    add custom neovim nvchad configs

---
+++ nvim/.config/nvim/lua/custom/mappings.lua
@@ -0,0 +1,21 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+  },
+  v = {
+    [">"] = { ">gv", "indent"},
+  },
+}
+
+-- more keybinds!
+
+M.disabled = {
+    i = {
+      ["<C-h>"] = "",
+    }
+}
+
+return M
